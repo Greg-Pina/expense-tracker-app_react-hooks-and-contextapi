de74ff26408b0d95c257b740fd2c7b5fce05bef4
Greg Pina <pinagm2825@gmail.com>
2020-07-15 16:00:43 -0400

    complete front end, need to hook up to database

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { Balance } from './components/Balance'
 import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList'
 import { AddTransaction } from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
 
 function App() {
 	return (
-		<div>
+		<GlobalProvider>
 			<Header />
 			<div className="container">
 				<Balance />
@@ -16,7 +17,7 @@ function App() {
 				<TransactionList />
 				<AddTransaction />
 			</div>
-		</div>
+		</GlobalProvider>
 	)
 }
 
