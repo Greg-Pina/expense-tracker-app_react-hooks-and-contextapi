f6ad718292b9329fbacb7607f7738465798754e2
Greg Pina <pinagm2825@gmail.com>
2020-07-23 18:26:35 -0400

    commas to income and expenses

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react'
 import { GlobalContext } from '../context/GlobalState'
+import { numberWithCommas } from '../utils/format'
 
 export const Transaction = ({ transaction }) => {
 	const { deleteTransaction } = useContext(GlobalContext)
@@ -9,7 +10,7 @@ export const Transaction = ({ transaction }) => {
 		<li className={transaction.amount < 0 ? 'minus' : 'plus'}>
 			{transaction.text}{' '}
 			<span>
-				{sign}${Math.abs(transaction.amount)}
+				{sign}${numberWithCommas(Math.abs(transaction.amount))}
 			</span>
 			<button
 				onClick={() => deleteTransaction(transaction._id)}
