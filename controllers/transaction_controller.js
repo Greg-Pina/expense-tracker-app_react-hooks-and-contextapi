ddf5150f9c9a6dcebf70d0b2289502eab8602d71
Greg Pina <pinagm2825@gmail.com>
2020-07-23 18:00:02 -0400

    axios to client folders, concurrently to scripts

--- controllers/transaction_controller.js
+++ controllers/transaction_controller.js
@@ -24,7 +24,25 @@ exports.getTransactions = async (req, res, next) => {
 // @access   Public
 exports.deleteTransactions = async (req, res, next) => {
 	try {
-	} catch (err) {}
+		const transaction = await Transaction.findById(req.params.id)
+
+		if (!transaction) {
+			return res.status(404).json({
+				success: false,
+				error: 'No transaction found',
+			})
+		}
+		await transaction.remove()
+		return res.status(200).json({
+			success: true,
+			data: {},
+		})
+	} catch (err) {
+		return res.status(500).json({
+			success: false,
+			error: 'Server Error',
+		})
+	}
 }
 
 // @desc     Add all transactions
